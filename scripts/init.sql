015302c21109cd3520bedd29461c756ce645ef7c
Jalen Adams <leftysolara@gmail.com>
2016-10-31 18:45:29 -0400

    add sql script for initializing database

---
+++ scripts/init.sql
@@ -0,0 +1,24 @@
+--
+-- File generated with SQLiteStudio v3.0.7 on Tue Oct 25 19:08:25 2016
+--
+-- Text encoding used: UTF-8
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: tasks
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE tasks (
+    id          INTEGER PRIMARY KEY ASC ON CONFLICT ROLLBACK,
+    description TEXT    NOT NULL ON CONFLICT ABORT,
+    due_date    DATE,
+    priority    INT,
+    status      BOOLEAN NOT NULL ON CONFLICT ROLLBACK
+                        DEFAULT (0),
+    tags        TEXT
+);
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
